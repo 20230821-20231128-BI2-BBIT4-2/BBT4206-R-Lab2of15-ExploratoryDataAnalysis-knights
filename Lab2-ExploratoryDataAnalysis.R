0e1eb29de7712c680a3d3930b4f1cd84a56acca4
PeterDavid3800 <davidpeter487@gmail.com>
2023-09-21 14:14:52 +0300

    Commit

--- Lab2-ExploratoryDataAnalysis.R
+++ Lab2-ExploratoryDataAnalysis.R
@@ -130,6 +130,9 @@ require("mlbench")
 data("PimaIndiansDiabetes")
 data("BostonHousing")
 
+library(readr)
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset <- read_csv("data/20230412-20230719-BI1-BBIT4-1-StudentPerformanceDataset.csv")
+View(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
 # Dimensions ----
 ## STEP 5. Preview the Loaded Datasets ----
 # Dimensions refer to the number of observations (rows) and the number of
@@ -140,6 +143,7 @@ dim(BostonHousing)
 dim(crop_dataset)
 dim(iris_dataset)
 dim(PimaIndiansDiabetes)
+dim(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
 
 # Data Types ----
 ## STEP 6. Identify the Data Types ----
@@ -152,6 +156,7 @@ sapply(BostonHousing, class)
 sapply(crop_dataset, class)
 sapply(iris_dataset, class)
 sapply(PimaIndiansDiabetes, class)
+sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset, class)
 
 # Descriptive Statistics ----
 
@@ -221,6 +226,25 @@ pima_indians_diabetes_freq <- PimaIndiansDiabetes$diabetes
 cbind(frequency = table(pima_indians_diabetes_freq),
       percentage = prop.table(table(pima_indians_diabetes_freq)) * 100)
 
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq <- X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$class_group
+cbind(frequency = table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq),
+      percentage = prop.table(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq)) *100)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq <- X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$alcohol_or_narcotics
+cbind(frequency = table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq),
+      percentage = prop.table(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq)) *100)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq <- X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$financial_wellness
+cbind(frequency = table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq),
+      percentage = prop.table(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq)) *100)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq <- X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$romantic_relationships
+cbind(frequency = table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq),
+      percentage = prop.table(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq)) *100)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq <- X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$gender
+cbind(frequency = table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq),
+      percentage = prop.table(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_freq)) *100)
 ## Measures of Central Tendency ----
 ### STEP 8. Calculate the mode ----
 # Unfortunately, R does not have an in-built function for calculating the mode.
@@ -247,6 +271,31 @@ pima_indians_diabetes_mode <- names(table(PimaIndiansDiabetes$diabetes))[
 ]
 print(pima_indians_diabetes_mode)
 
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_class_group_mode <- names(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$class_group))[
+  which(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$class_group) == max(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$class_group)))
+]
+print(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_class_group_mode)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_alcohol_or_narcotics_mode <- names(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$alcohol_or_narcotics))[
+  which(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$alcohol_or_narcotics) == max(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$alcohol_or_narcotics)))
+]
+print(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_alcohol_or_narcotics_mode)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_financial_wellness_mode <- names(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$financial_wellness))[
+  which(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$financial_wellness) == max(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$financial_wellness)))
+]
+
+print(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_financial_wellness_mode)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_romantic_relationships_mode <- names(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$romantic_relationships))[
+  which(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$romantic_relationships) == max(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$romantic_relationships)))
+]
+print(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_romantic_relationships_mode)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_gender_mode <- names(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$gender))[
+  which(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$gender) == max(table(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset$gender)))
+]
+print(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_gender_mode)
 ## Measures of Distribution/Dispersion/Spread/Scatter/Variability ----
 
 ### STEP 9. Measure the distribution of the data for each variable ----
@@ -254,6 +303,7 @@ summary(BostonHousing)
 summary(crop_dataset)
 summary(iris_dataset)
 summary(PimaIndiansDiabetes)
+summary(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset)
 
 ### STEP 10. Measure the standard deviation of each variable ----
 # Measuring the variability in the dataset is important because the amount of
@@ -282,12 +332,14 @@ sapply(BostonHousing[, c(1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)], sd)
 sapply(crop_dataset[, 4], sd)
 sapply(iris_dataset[, 1:4], sd)
 sapply(PimaIndiansDiabetes[, 1:8], sd)
+sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[, c(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99)], sd)
 
 ### STEP 11. Measure the variance of each variable ----
 sapply(BostonHousing[, -4], var)
 sapply(crop_dataset[, 4], var)
 sapply(iris_dataset[, 1:4], var)
 sapply(PimaIndiansDiabetes[, 1:8], var)
+sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset[, c(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99)], var)
 
 ### STEP 12. Measure the kurtosis of each variable ----
 # The Kurtosis informs you of how often outliers occur in the results.
@@ -311,6 +363,7 @@ sapply(BostonHousing[, -4],  kurtosis, type = 2)
 sapply(crop_dataset[, 4],  kurtosis, type = 2)
 sapply(iris_dataset[, 1:4],  kurtosis, type = 2)
 sapply(PimaIndiansDiabetes[, 1:8],  kurtosis, type = 2)
+sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset [c(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99)],kurtosis, type=2)
 
 ### STEP 13. Measure the skewness of each variable ----
 
@@ -328,6 +381,8 @@ sapply(BostonHousing[, -4],  skewness, type = 2)
 sapply(crop_dataset[, 4],  skewness, type = 2)
 sapply(iris_dataset[, 1:4],  skewness, type = 2)
 sapply(PimaIndiansDiabetes[, 1:8],  skewness, type = 2)
+sapply(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset [c(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99)],skewness, type=2)
+
 
 # Note, executing:
 # skewness(BostonHousing$crim, type=2) # nolint
@@ -354,6 +409,8 @@ View(iris_dataset_cov)
 pima_indians_diabetes_cov <- cov(PimaIndiansDiabetes[, 1:8])
 View(pima_indians_diabetes_cov)
 
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_cov <- cov(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset [c(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99)])
+View(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_cov)
 ## STEP 15. Measure the correlation between variables ----
 boston_housing_cor <- cor(BostonHousing[, -4])
 View(boston_housing_cor)
@@ -366,6 +423,9 @@ View(iris_dataset_cor)
 
 pima_indians_diabetes_cor <- cor(PimaIndiansDiabetes[, 1:8])
 View(pima_indians_diabetes_cor)
+
+X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_cor <- cor(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset [c(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99)])
+View(X20230412_20230719_BI1_BBIT4_1_StudentPerformanceDataset_cor)
 
 # Inferential Statistics ----
 # Read the following article:
